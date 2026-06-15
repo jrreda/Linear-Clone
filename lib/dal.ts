4969238d6dc5a72734673990148198569aaf161c
Reda <mahmoudreda457@gmail.com>
2026-06-15 07:26:29 +0300

    Organization

--- lib/dal.ts
+++ lib/dal.ts
@@ -1,8 +1,8 @@
-import { db } from '@/app/db'
+import { db } from '../../../db'
 import { getSession } from './auth'
 import { eq } from 'drizzle-orm'
 import { cache } from 'react'
-import { issues, users } from '@/app/db/schema'
+import { issues, users } from '../../../db/schema'
 import { mockDelay } from './utils'
 
 export const getUserByEmail = () => {}
